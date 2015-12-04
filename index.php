991c881694c4f188910367b6e50d79f1d67d9ed5
MerrittofD4 <merritt@d4webdesign.com>
2015-12-04 09:31:06 -0700

    Initial plugin creation
    Added basic functionality and files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Plugin Name: D4 Swipebox
+	Plugin URI: http://brutaldesign.github.io/swipebox/#basic
+	GitHub Theme URI: http://brutaldesign.github.io/swipebox/#basic
+	GitHub Branch: master
+	Description: Simple Swipebox Plugin
+	Version: 11Oct15
+	Author: D4 Adv. Media (Plugin) Brutal Design (Original Code)
+	License: GPL2
+*/
+
+// Register Script
+
+wp_register_script( 'swipebox', plugins_url( '/src/js/jquery.swipebox.min.js' , __FILE__ ), array( 'jquery' ), 'v20131005', true );
+wp_enqueue_script('swipebox');
+
+wp_register_style( 'swipebox', plugins_url( '/src/css/swipebox.css' , __FILE__ ));
+wp_enqueue_style('swipebox' );
+
+wp_register_script( 'swipebox-init', plugins_url( '/swipebox-init.js' , __FILE__ ), array( 'jquery' ), 'v20131005', true );
+wp_enqueue_script('swipebox-init');
+
+?>
